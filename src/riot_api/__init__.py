f07f91c6037fb504e63ee013ab4dab5c72d8b0a1
Andrew <andrew.sanetra@gmail.com>
2016-03-13 01:49:23 -0600

    Add doc string & additional classes

--- src/riot_api/__init__.py
+++ src/riot_api/__init__.py
@@ -1,1 +1,9 @@
+"""
+Make all classes available for import from riot_api
+e.g. ( from .module import Class )
+"""
+
 from .RiotAPI import RiotAPI
+from .Matches import Match
+from .Summoners import Summoner
+from .Summoners import SummonerStats
